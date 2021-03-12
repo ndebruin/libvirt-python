4e16c6989796f160dbd5cd3e6264032f4ef6918b
ndebruin <nzdebruin@tutanota.com>
2021-03-12 15:54:47 -0500

    added template for libvirt connections

---
+++ template.py
@@ -0,0 +1,14 @@
+import libvirt
+
+LIBVIRT_URI = "qemu+ssh://ndebruin@192.168.22.231/system"
+
+
+client = libvirt.openReadOnly(LIBVIRT_URI)
+if client == None:
+    print("Failed to open connection to " + LIBVIRT_URI)
+    exit(1)
+
+
+
+client.close()
+exit(0)
