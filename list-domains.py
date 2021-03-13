cc18799261dbec32120b6d968b0eea904fc0a479
ndebruin <nzdebruin@tutanota.com>
2021-03-12 20:53:42 -0500

    formatting of outputs

--- list-domains.py
+++ list-domains.py
@@ -1,6 +1,7 @@
+from tabulate import tabulate
 import libvirt
 
-LIBVIRT_URI = "qemu+ssh://ndebruin@192.168.22.231/system"
+LIBVIRT_URI = "qemu+ssh://192.168.22.231/system"
 
 
 client = libvirt.openReadOnly(LIBVIRT_URI)
@@ -8,12 +9,16 @@ if client == None:
     print("Failed to open connection to " + LIBVIRT_URI)
     exit(1)
 
-
-
-
-
-
-
+print("List of All VMs on the Host:")
+domains = client.listAllDomains(0)
+if len(domains) != 0:
+    for domain in domains:
+        if domain.isActive() == 1:
+            #print("     "+domain.name()+"       Running")
+            print(tabulate([[domain.name(), "Running"]], headers=["Name", "Status"]))
+        else:
+            print(tabulate([[domain.name(), "Stopped"]]))
+            #print("     "+domain.name()+"       Stopped")
 
 client.close()
 exit(0)
